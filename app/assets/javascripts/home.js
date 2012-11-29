a4efb60fca28d55296639a0a52b399090ff28657
njerichelimo <martha.c.chumo@gmail.com>
2012-11-29 16:29:52 +0300

    more js fixes

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -6,71 +6,7 @@ $(document).ready(function(){
         columnWidth: 100
       });
     });
-    $container.infinitescroll({
-      navSelector  : '#page-nav',    // selector for the paged navigation
-      nextSelector : '#page-nav a',  // selector for the NEXT link (to page 2)
-      itemSelector : '.box',     // selector for all items you'll retrieve
-      loading: {
-          finishedMsg: 'No more pages to load.',
-          img: 'http://i.imgur.com/6RMhx.gif'
-        }
-      }),
-      function( newElements ) {
-        var $newElems = $( newElements ).css({ opacity: 0 });
-        $newElems.imagesLoaded(function(){
-          $newElems.animate({ opacity: 1 });
-          $container.masonry( 'appended', $newElems, true );
-        });
-      }
-    });
 
-
-
-
-
-
-
-
-
-//$(function(){
-  //var $container = $('#all_images');
-  //$container.imagesLoaded( function(){
-    //$container.masonry({
-      //itemSelector : '.image',
-      //columnWidth : 100,
-      //isAnimated: true
-    //});
-  //});
-
-//  $container.infinitescroll({
-//          loading: {
-//              finishedMsg: 'No more pages to load.',
-//              //img: 'http://i.imgur.com/6RMhx.gif'
-//            }
-//          },
-//          // trigger Masonry as a callback
-//          function( newElements ) {
-//            // hide new items while they are loading
-//            var $newElems = $( newElements ).css({ opacity: 0 });
-//            // ensure that images load before adding to masonry layout
-//            $newElems.imagesLoaded(function(){
-//              // show elems now they're ready
-//              $newElems.animate({ opacity: 1 });
-//              $container.masonry( 'appended', $newElems, true );
-//            });
-//          }
-//        );
-
-
-
-//
-//});
-
-
-
-
-
-$(document).ready(function() {
      $('#myButton').click(function(e) {
           e.preventDefault();
 	  $('#myModal').reveal({
@@ -78,5 +14,6 @@ $(document).ready(function() {
      animationspeed: 300,
      closeonbackgroundclick: true,
      dismissmodalclass: 'close-reveal-modal'
+    });
   });
 });
