8538ddc8be8fd91f3320c724dfa3da348b2cb18d
njerichelimo <martha.c.chumo@gmail.com>
2012-12-03 17:13:53 +0300

    correcting imagemagick path

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,5 +45,5 @@ Giga::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
-  Paperclip.options[:command_path] = "/usr/local/bin/"
+  Paperclip.options[:command_path] = "/usr/bin/"
 end
