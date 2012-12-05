f61f15a2494da8efea573b1ce80264d6f3c141d9
njerichelimo <martha.c.chumo@gmail.com>
2012-12-05 14:49:49 +0300

    restructuring registration od admin

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -35,7 +35,7 @@ class RegistrationsController < Devise::RegistrationsController
           if @user.update_attributes(user_update)
             mpayer_org_no = user_details["org_no"]
             mpayer_org_name = user_details["org_name"]
-            unless Organization.where({:org_no => mpayer_org_no})
+            unless @org = Organization.where({:org_no => mpayer_org_no})
               @org = Organization.create!({:org_no => mpayer_org_no, :name => mpayer_org_name})
             end
             @user.update_attributes({:organization_id => @org.id})
