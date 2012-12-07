00a1877129035510e5661c770b718497fc086567
njerichelimo <martha.c.chumo@gmail.com>
2012-12-07 14:18:28 +0300

    reverted to first code for registrations_controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,52 +9,35 @@ class RegistrationsController < Devise::RegistrationsController
 
   def create
     @user = User.new(params[:user])
-    #if User.where({:name => @user.name})
-    #if User.where({:name => @user.name})
-      #respond_to do |format|
-        #format.html { redirect_to new_user_session_path :notice => "Please log in"}
-        #format.json { render :json => @user, status: :created, location: @admin_user }
-      #end
-    unless User.where({:name => @user.name})
-      respond_to do |format|
-        sign_in(admin)
-        format.html { redirect_to root_url }
-        format.json { render :json => admin }
-      end
-    else
-      respond_to do |format|
-        user_details = @user.synchronize_mpayer(@user.name, @user.password)
-        mpayer_user_id = user_details["user_id"]
-        token = user_details["token"]
-        decrypted_token = Base64.decode64(token)
-        mpayer_user_no = user_details["user_no"]
-        mpayer_user_name = user_details["user_name"]
-        mpayer_user_role = user_details["user_role"]
-        user_update = {:name => mpayer_user_name, :mpayer_user_id => mpayer_user_id, :mpayer_user_no => mpayer_user_no, :role => mpayer_user_role, :token => decrypted_token}
-        if @user.save!
-          if @user.update_attributes(user_update)
-            mpayer_org_no = user_details["org_no"]
-            mpayer_org_name = user_details["org_name"]
-            unless @org = Organization.where({:org_no => mpayer_org_no})
-              @org = Organization.create!({:org_no => mpayer_org_no, :name => mpayer_org_name})
-            end
-            @user.update_attributes({:organization_id => @org.id})
-            @user.save!
-            @mpayer = Mpayer.new(mpayer_user_no, decrypted_token)
-            @ac = @mpayer.mpayer_account
-            accounts = @ac.all_accounts
-            acs = JSON.parse(accounts)
-            acs.each do |a|
-              @ac = @org.accounts.create!(a)
-            end
+    respond_to do |format|
+      user_details = @user.synchronize_mpayer(@user.name, @user.password)
+      mpayer_user_id = user_details["user_id"]
+      token = user_details["token"]
+      decrypted_token = Base64.decode64(token)
+      mpayer_user_no = user_details["user_no"]
+      mpayer_user_name = user_details["user_name"]
+      mpayer_user_role = user_details["user_role"]
+      user_update = {:name => mpayer_user_name, :mpayer_user_id => mpayer_user_id, :mpayer_user_no => mpayer_user_no, :role => mpayer_user_role, :token => decrypted_token}
+      if @user.save!
+        if @user.update_attributes(user_update)
+          org_no = user_details["org_no"]
+          @org = Organization.find_or_create_by({:org_no => org_no})
+          @user.update_attributes({:organization_id => @org.id})
+          @user.save!
+          @mpayer = Mpayer.new(mpayer_user_no, decrypted_token)
+          @ac = @mpayer.mpayer_account
+          accounts = @ac.all_accounts
+          acs = JSON.parse(accounts)
+          acs.each do |a|
+            @ac = @org.accounts.find_or_create_by(a)
           end
-          sign_in(@user)
-          format.html { redirect_to root_url }
-          format.json { render :json => @user, status: :created, location: @admin_user }
-        else
-          format.html { render action: "new" }
-          format.json { render :json => @user.errors, status: :unprocessable_entity }
         end
+        sign_in(@user)
+        format.html { redirect_to root_url }
+        format.json { render :json => @user, status: :created, location: @admin_user }
+      else
+        format.html { render action: "new" }
+        format.json { render :json => @user.errors, status: :unprocessable_entity }
       end
     end
   end
